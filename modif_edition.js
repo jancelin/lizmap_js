9f1c8668286f4b8169044f4287c089e30591c271
ANCELIN Julien <julien.ancelin@inra.fr>
2017-04-03 16:29:10 +0200

    Update modif_edition.js

--- modif_edition.js
+++ modif_edition.js
@@ -4,30 +4,30 @@
  // Creation
             if( action == 'createFeature' ){
 
->>                // Activate drawFeature control only if relevant
->>                if(  
->> editionLayer['config'].capabilities.createFeature == "True"
->>                && geometryType in editCtrls ){
->>                    var ctrl = editCtrls[geometryType];
->>                    if ( ctrl.active ) {
->>                        return false;
->>                    } else {
->>                        ctrl.activate();
->>
->> $('#attribution-box').click(function(){
->> alert('gps working...');
->> navigator.geolocation.getCurrentPosition(function(position) {
->> var lat = position.coords.latitude;
->> var lon = position.coords.longitude;
->> var point = new OpenLayers.Geometry.Point(lon, lat).transform(new  
->> OpenLayers.Projection("EPSG:4326"), map.getProjectionObject());
->> var point_ft = new OpenLayers.Feature.Vector(point, null, null);
->> editionLayer['ol'].addFeatures([point_ft]);
->> alert('finish');
->> });
->> });
->>                        $('#lizmap-edition-message').remove();
->> lizMap.addMessage(lizDict['edition.draw.activate'],'info',true).attr('id','lizmap-edition-message');
->>                    }
->>                }
->>            }
+                // Activate drawFeature control only if relevant
+                if(  
+ editionLayer['config'].capabilities.createFeature == "True"
+                && geometryType in editCtrls ){
+                    var ctrl = editCtrls[geometryType];
+                    if ( ctrl.active ) {
+                        return false;
+                    } else {
+                        ctrl.activate();
+
+ $('#attribution-box').click(function(){
+ alert('gps working...');
+ navigator.geolocation.getCurrentPosition(function(position) {
+ var lat = position.coords.latitude;
+ var lon = position.coords.longitude;
+ var point = new OpenLayers.Geometry.Point(lon, lat).transform(new  
+ OpenLayers.Projection("EPSG:4326"), map.getProjectionObject());
+ var point_ft = new OpenLayers.Feature.Vector(point, null, null);
+ editionLayer['ol'].addFeatures([point_ft]);
+ alert('finish');
+ });
+ });
+                        $('#lizmap-edition-message').remove();
+ lizMap.addMessage(lizDict['edition.draw.activate'],'info',true).attr('id','lizmap-edition-message');
+                    }
+                }
+            }
